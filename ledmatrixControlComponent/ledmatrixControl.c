7c4705d9dd0c6e1296772c2326ecabcbab17d0dd
Thong Nguyen <nxthongbk@gmail.com>
2019-04-10 14:52:48 +0700

    improve

--- ledmatrixControlComponent/ledmatrixControl.c
+++ ledmatrixControlComponent/ledmatrixControl.c
@@ -19,19 +19,15 @@ static le_timer_Ref_t Timer;
 //--------------------------------------------------------------------------------------------------
 static void TimerExpired
 (
-    le_timer_Ref_t timer    ///< Timer reference
+	le_timer_Ref_t timer    ///< Timer reference
 )
-//--------------------------------------------------------------------------------------------------
 {
-    static unsigned int counter = 0;
-    int32_t number = 0;
+	static unsigned int counter = 0;
+	int32_t number = 0;
  
-    counter += 1;
-
-    number += counter % 200;
-
-    io_PushNumeric(LEDMATRIX_NAME, IO_NOW, number);
-   
+	counter += 1;
+	number += counter % 200;
+	io_PushNumeric(LEDMATRIX_NAME, IO_NOW, number);
 }
 
 
@@ -43,30 +39,30 @@ static void TimerExpired
 //--------------------------------------------------------------------------------------------------
 static void PeriodUpdateHandler
 (
-    double timestamp,   ///< time stamp
-    double value,       ///< period value, seconds
-    void* contextPtr    ///< not used
+	double timestamp,	///< time stamp
+	double value,		///< period value, seconds
+	void* contextPtr	///< not used
 )
 //--------------------------------------------------------------------------------------------------
 {
-    LE_INFO("Received update to 'period' setting: %lf (timestamped %lf)", value, timestamp);
+	LE_INFO("Received update to 'period' setting: %lf (timestamped %lf)", value, timestamp);
 
-    uint32_t ms = (uint32_t)(value * 1000);
+	uint32_t ms = (uint32_t)(value * 1000);
 
-    if (ms == 0)
-    {
-        le_timer_Stop(Timer);
-    }
-    else
-    {
-        le_timer_SetMsInterval(Timer, ms);
+	if (ms == 0)
+	{
+		le_timer_Stop(Timer);
+	}
+	else
+	{
+		le_timer_SetMsInterval(Timer, ms);
 
-        // If the sensor is enabled and the timer is not already running, start it now.
-        if (IsEnabled && (!le_timer_IsRunning(Timer)))
-        {
-            le_timer_Start(Timer);
-        }
-    }
+		// If the sensor is enabled and the timer is not already running, start it now.
+		if (IsEnabled && (!le_timer_IsRunning(Timer)))
+		{
+			le_timer_Start(Timer);
+		}
+    	}
 }
 
 
@@ -78,30 +74,30 @@ static void PeriodUpdateHandler
 //--------------------------------------------------------------------------------------------------
 static void EnableUpdateHandler
 (
-    double timestamp,   ///< time stamp
-    bool value,         ///< whether input is enabled
-    void* contextPtr    ///< not used
+	double timestamp,	///< time stamp
+	bool value,		///< whether input is enabled
+	void* contextPtr	///< not used
 )
 //--------------------------------------------------------------------------------------------------
 {
-    LE_INFO("Received update to 'enable' setting: %s (timestamped %lf)",
-            value == false ? "false" : "true",
-            timestamp);
+	LE_INFO("Received update to 'enable' setting: %s (timestamped %lf)",
+ 		value == false ? "false" : "true",
+		timestamp);
 
-    IsEnabled = value;
+	IsEnabled = value;
 
-    if (value)
-    {
-        // If the timer has a non-zero interval and is not already running, start it now.
-        if ((le_timer_GetMsInterval(Timer) != 0) && (!le_timer_IsRunning(Timer)))
-        {
-            le_timer_Start(Timer);
-        }
-    }
-    else
-    {
-        le_timer_Stop(Timer);
-    }
+	if (value)
+	{
+		// If the timer has a non-zero interval and is not already running, start it now.
+		if ((le_timer_GetMsInterval(Timer) != 0) && (!le_timer_IsRunning(Timer)))
+		{
+			le_timer_Start(Timer);
+		}
+	}
+	else
+	{
+		le_timer_Stop(Timer);
+	}
 }
 
 
@@ -112,47 +108,46 @@ static void EnableUpdateHandler
 //--------------------------------------------------------------------------------------------------
 static void UpdateStartEndHandler
 (
-    bool isStarting,    //< input is starting
-    void* contextPtr    //< Not used.
+	bool isStarting,    //< input is starting
+	void* contextPtr    //< Not used.
 )
-//--------------------------------------------------------------------------------------------------
 {
-    LE_INFO("Configuration update %s.", isStarting ? "starting" : "finished");
+	LE_INFO("Configuration update %s.", isStarting ? "starting" : "finished");
 }
 
 
 COMPONENT_INIT
 {
-    le_result_t result;
+	le_result_t result;
 
-    io_AddUpdateStartEndHandler(UpdateStartEndHandler, NULL);
+	io_AddUpdateStartEndHandler(UpdateStartEndHandler, NULL);
 
-    // This will be provided to the Data Hub.
-    result = io_CreateInput(LEDMATRIX_NAME, IO_DATA_TYPE_NUMERIC, "degrees");
-    LE_ASSERT(result == LE_OK);
+	// This will be provided to the Data Hub.
+	result = io_CreateInput(LEDMATRIX_NAME, IO_DATA_TYPE_NUMERIC, "degrees");
+	LE_ASSERT(result == LE_OK);
     
 
-    // This is my configuration setting.
-    result = io_CreateOutput(PERIOD_NAME, IO_DATA_TYPE_NUMERIC, "s");
-    LE_ASSERT(result == LE_OK);
+	// This is my configuration setting.
+	result = io_CreateOutput(PERIOD_NAME, IO_DATA_TYPE_NUMERIC, "s");
+	LE_ASSERT(result == LE_OK);
 
-    // Register for notification of updates to our configuration setting.
-    io_AddNumericPushHandler(PERIOD_NAME, PeriodUpdateHandler, NULL);
+	// Register for notification of updates to our configuration setting.
+	io_AddNumericPushHandler(PERIOD_NAME, PeriodUpdateHandler, NULL);
 
-    // This is my enable/disable control.
-    result = io_CreateOutput(ENABLE_NAME, IO_DATA_TYPE_BOOLEAN, "");
-    LE_ASSERT(result == LE_OK);
+	// This is my enable/disable control.
+	result = io_CreateOutput(ENABLE_NAME, IO_DATA_TYPE_BOOLEAN, "");
+	LE_ASSERT(result == LE_OK);
 
-    // Set the defaults: enable the sensor, set period to 1s
-    io_SetBooleanDefault(ENABLE_NAME, true);
-    io_SetNumericDefault(PERIOD_NAME, 1);
+	// Set the defaults: enable the sensor, set period to 1s
+	io_SetBooleanDefault(ENABLE_NAME, true);
+	io_SetNumericDefault(PERIOD_NAME, 1);
 
-    // Register for notification of updates to our enable/disable control.
-    io_AddBooleanPushHandler(ENABLE_NAME, EnableUpdateHandler, NULL);
+	// Register for notification of updates to our enable/disable control.
+	io_AddBooleanPushHandler(ENABLE_NAME, EnableUpdateHandler, NULL);
 
-    // Create a repeating timer that will call TimerExpired() each time it expires.
-    // Note: we'll start the timer when we receive our configuration setting.
-    Timer = le_timer_Create("ledmatrixTimer");
-    le_timer_SetRepeat(Timer, 0);
-    le_timer_SetHandler(Timer, TimerExpired);
+	// Create a repeating timer that will call TimerExpired() each time it expires.
+	// Note: we'll start the timer when we receive our configuration setting.
+	Timer = le_timer_Create("ledmatrixTimer");
+	le_timer_SetRepeat(Timer, 0);
+	le_timer_SetHandler(Timer, TimerExpired);
 }
--- ledmatrixDisplayComponent/ledmatrixDisplay.c
+++ ledmatrixDisplayComponent/ledmatrixDisplay.c
@@ -7,11 +7,8 @@
 
 #define LEDMATRIX_OBS "ledmatrixOffLimits"
 
-
 #define LEDMATRIX_LOWER_LIMIT 50.000000
-#define LEDMATRIX_UPPER_LIMIT 50.000100
-
-
+#define LEDMATRIX_UPPER_LIMIT 50.000000
 
 //--------------------------------------------------------------------------------------------------
 /**
@@ -20,73 +17,65 @@
 //--------------------------------------------------------------------------------------------------
 static void LedmatrixUpdateHandler
 (
-    double timestamp,   ///< time stamp
-    double value,       ///< latitude value, degrees
-    void* contextPtr    ///< not used
+	double timestamp,
+	double value,
+	void* contextPtr
 )
 {
-    LE_DEBUG("ledmatrix = %lf (timestamped %lf)", value, timestamp);
+	LE_DEBUG("ledmatrix = %lf (timestamped %lf)", value, timestamp);
 }
-
 
 
 //--------------------------------------------------------------------------------------------------
 /**
- * Call-back function called when an update is received from the Data Hub for the latitude value.
+ * Call-back function called when an update is received from the Data Hub for the led matrix value.
  */
 //--------------------------------------------------------------------------------------------------
 static void LedmatrixObservationUpdateHandler
 (
-    double timestamp,   ///< time stamp
-    double value,       ///< latitude value, degrees
-    void* contextPtr    ///< not used
+	double timestamp,	///< time stamp
+	double value,		///< ledmatrix value,
+	void* contextPtr	///< not used 
 )
 {
-    LE_INFO("Observed filtered led matrix = %lf (timestamped %lf)", value, timestamp);
+	LE_INFO("Observed filtered led matrix = %lf (timestamped %lf)", value, timestamp);
 
-    char led_value[50];
-    snprintf(led_value, 50, "%f", value);
+	char led_value[50];
+	snprintf(led_value, 50, "%f", value);
 
-    ma_ledmatrix_displayString(led_value, 10000, true, MA_LEDMATRIX_green);
+	ma_ledmatrix_displayString(led_value, 10000, true, MA_LEDMATRIX_green);
 
 }
 
 
 COMPONENT_INIT
 {
-    
-    le_result_t result;
-    ma_ledmatrix_init("/dev/i2c-0", 0, 0);
-   
-    uint16_t pid = ma_ledmatrix_getDevicePID();
-    uint16_t vid = ma_ledmatrix_getDeviceVID();
-    LE_INFO("PID: %x; VID: %x\n", pid, vid);
+	le_result_t result;
+	ma_ledmatrix_init("/dev/i2c-0", 0, 0);
+	
+	uint16_t pid = ma_ledmatrix_getDevicePID();
+	uint16_t vid = ma_ledmatrix_getDeviceVID();
+	LE_INFO("PID: %x; VID: %x\n", pid, vid);
 
+	// This will be received from the Data Hub.
+	result = io_CreateOutput(LEDMATRIX_NAME, IO_DATA_TYPE_NUMERIC, "degrees");
+	LE_ASSERT(result == LE_OK);
 
-    // This will be received from the Data Hub.
-    result = io_CreateOutput(LEDMATRIX_NAME, IO_DATA_TYPE_NUMERIC, "degrees");
-    LE_ASSERT(result == LE_OK);
-  
+	// Register for notification of updates to the counter value.
+	io_AddNumericPushHandler(LEDMATRIX_NAME, LedmatrixUpdateHandler, NULL);
 
-    // Register for notification of updates to the counter value.
-    io_AddNumericPushHandler(LEDMATRIX_NAME, LedmatrixUpdateHandler, NULL);
-  
+	// Connect to the sensor
+	result = admin_SetSource("/app/ledmatrixDisplay/" LEDMATRIX_NAME, LEDMATRIX_CONTROL);
+	LE_ASSERT(result == LE_OK);
 
-    // Connect to the sensor
-    result = admin_SetSource("/app/ledmatrixDisplay/" LEDMATRIX_NAME, LEDMATRIX_CONTROL);
-    LE_ASSERT(result == LE_OK);
-   
+	// Create observation (filter) for ledmatrix. To set up the "dead band" filter,
+	// lower limit assigned to high limit and vice versa (see admin.io doc for details).
+	admin_CreateObs(LEDMATRIX_OBS);
+	admin_SetLowLimit(LEDMATRIX_OBS, LEDMATRIX_UPPER_LIMIT);
+	admin_SetHighLimit(LEDMATRIX_OBS, LEDMATRIX_LOWER_LIMIT);
+	result = admin_SetSource("/obs/" LEDMATRIX_OBS, LEDMATRIX_CONTROL);
+	LE_ASSERT(result == LE_OK);
+	admin_AddNumericPushHandler("/obs/" LEDMATRIX_OBS, LedmatrixObservationUpdateHandler, NULL);
 
-    // Create observation (filter) for ledmatrix. To set up the "dead band" filter,
-    // lower limit assigned to high limit and vice versa (see admin.io doc for details).
-    admin_CreateObs(LEDMATRIX_OBS);
-    admin_SetLowLimit(LEDMATRIX_OBS, LEDMATRIX_UPPER_LIMIT);
-    admin_SetHighLimit(LEDMATRIX_OBS, LEDMATRIX_LOWER_LIMIT);
-    result = admin_SetSource("/obs/" LEDMATRIX_OBS, LEDMATRIX_CONTROL);
-    LE_ASSERT(result == LE_OK);
-    admin_AddNumericPushHandler("/obs/" LEDMATRIX_OBS, LedmatrixObservationUpdateHandler, NULL);
-
-    LE_ASSERT(result == LE_OK);
-
-
+	LE_ASSERT(result == LE_OK);
 }
